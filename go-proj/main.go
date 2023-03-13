f57b902fcb8d40cecec19b31c0510736b47b6e1a
Yogesh Kumar <yogesh.kumar@trilogy.com>
2023-03-13 13:52:54 +0530

    wip

--- go-proj/main.go
+++ go-proj/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/mafredri/cdp"
@@ -13,6 +14,10 @@ import (
 	"github.com/mafredri/cdp/protocol/debugger"
 	"github.com/mafredri/cdp/rpcc"
 )
+
+var client *cdp.Client
+var pausedClient debugger.PausedClient
+var wg sync.WaitGroup
 
 func run(timeout time.Duration) error {
 	// ctx, cancel := context.WithTimeout(context.Background(), timeout)
@@ -34,43 +39,61 @@ func run(timeout time.Duration) error {
 	}
 	defer conn.Close() // Leaving connections open will leak memory.
 
-	client := cdp.NewClient(conn)
+	client = cdp.NewClient(conn)
 
 	// Enable debugging features
 	_, err = client.Debugger.Enable(ctx, &debugger.EnableArgs{})
 	if err != nil {
 		return err
 	}
-	
+
 	curDir, _ := os.Getwd()
 	parDir := filepath.Dir(curDir)
 	urlRegex := "^.*" + parDir + "/my-project2/node_modules/constructs/lib/construct.js$"
 	columnNumber := 9
 	client.Debugger.SetBreakpointByURL(ctx, &debugger.SetBreakpointByURLArgs{
-		URLRegex: &urlRegex,
-		LineNumber: 368,
+		URLRegex:     &urlRegex,
+		LineNumber:   368,
 		ColumnNumber: &columnNumber,
 	})
 
-	pausedClient, err := client.Debugger.Paused(ctx)
+	pausedClient, err = client.Debugger.Paused(ctx)
 	client.Debugger.Resume(ctx, &debugger.ResumeArgs{})
 	err = client.Runtime.RunIfWaitingForDebugger(ctx)
 
 	if err != nil {
 		return err
 	}
-	
-	for {
-		
-		ev, err := pausedClient.Recv()
-		if err != nil {
-			return err
-		}
-		fmt.Println(ev.Reason)
-		client.Debugger.Resume(ctx, &debugger.ResumeArgs{})
-		// fmt.Println(ev)
-	}
 
+	wg.Add(1)
+	go parseBreakpointData(ctx)
+
+	wg.Wait()
+	return nil
+}
+
+func parseBreakpointData(ctx context.Context) error {
+	defer wg.Done()
+	ev, err := pausedClient.Recv()
+	if err != nil {
+		return err
+	}
+	fmt.Println(ev.Reason)
+
+	for _, callFrame := range ev.CallFrames {
+		fmt.Println(callFrame.URL)
+		for _, scope := range callFrame.ScopeChain {
+			if scope.Object.Value == nil && scope.Object.UnserializableValue == nil && scope.Object.WebDriverValue == nil {
+				continue
+			}
+			fmt.Println("ClassName: ", scope.Object.ClassName)
+			fmt.Println("Description: ", scope.Object.Description)
+			fmt.Println("UnsValue: ", scope.Object.UnserializableValue)
+		}
+	}
+	client.Debugger.Resume(ctx, &debugger.ResumeArgs{})
+	wg.Add(1)
+	go parseBreakpointData(ctx)
 	return nil
 }
 
