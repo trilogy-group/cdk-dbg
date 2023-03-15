3a7b191eac0fb93150586960faa41e183552e956
Ankushpandey-ti <ankush.pandey@trilogy.com>
2023-03-15 15:52:58 +0530

    tried extracting userful data in a map

--- go-proj/main.go
+++ go-proj/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/mafredri/cdp"
 	"github.com/mafredri/cdp/devtool"
 	"github.com/mafredri/cdp/protocol/debugger"
+	"github.com/mafredri/cdp/protocol/runtime"
 	"github.com/mafredri/cdp/rpcc"
 )
 
@@ -79,18 +80,62 @@ func parseBreakpointData(ctx context.Context) error {
 		return err
 	}
 	fmt.Println(ev.Reason)
+	var Id_Scope map[string][]string
 
-	for _, callFrame := range ev.CallFrames {
-		fmt.Println(callFrame.URL)
-		for _, scope := range callFrame.ScopeChain {
-			if scope.Object.Value == nil && scope.Object.UnserializableValue == nil && scope.Object.WebDriverValue == nil {
-				continue
-			}
-			fmt.Println("ClassName: ", scope.Object.ClassName)
-			fmt.Println("Description: ", scope.Object.Description)
-			fmt.Println("UnsValue: ", scope.Object.UnserializableValue)
-		}
+	var functionNames = map[string]string{
+		"Construct":"Construct",
 	}
+	for indexCF, callFrame := range ev.CallFrames {
+		fmt.Println(callFrame.URL)
+		val,ok := functionNames[callFrame.FunctionName]
+
+		// To see what data is being retuned in z visit 
+		//https://pkg.go.dev/github.com/mafredri/cdp@v0.33.0/protocol/runtime#PropertyDescriptor
+		//https://pkg.go.dev/github.com/mafredri/cdp@v0.33.0/protocol/runtime#GetPropertiesReply
+		if ok && ev.Reason !="Break on start"{
+			fmt.Print("FUNCTION NAME FOUND ",val," INDEX OF CF ",indexCF)
+			z, _ := client.Runtime.GetProperties(ctx, &runtime.GetPropertiesArgs{ObjectID: *callFrame.ScopeChain[0].Object.ObjectID})
+			for ind,data := range z.Result{
+				fmt.Print("\nDATA FOR INDEX ",ind)
+				fmt.Print("\nDATA = ",data)
+				fmt.Print("\n Data.Value =",data.Value)
+				if data.Value.ClassName !=nil{ 
+					fmt.Print("\n Data.Value.Class ",*data.Value.ClassName)
+					_,ok := Id_Scope[data.Name]
+				if ok{
+					var res []string 
+					res = append(res, *data.Value.Description)
+					Id_Scope[data.Name] = res
+				}else{
+					Id_Scope[data.Name] = append(Id_Scope[data.Name], *data.Value.Description)
+				}
+				}
+				if data.Value.Description != nil{
+					fmt.Print("\n Data.Value.Description ",*data.Value.Description)
+				}
+				fmt.Print("\n Data.Value.Type ",data.Value.Type)
+				fmt.Print("\n Data.Value.Preview ",data.Value.Preview)
+				fmt.Print("\n Data.Value.CustomPreview ",data.Value.CustomPreview)
+				
+					
+
+
+				
+
+				// scope,ok := Id_Scope[data.Name]
+				// if ok{
+				// 	var val []string 
+				// 	val = append(val, string(*data.Value.ClassName))
+				// 	Id_Scope[data.Name] = val
+				// }
+				// fmt.Print("\nWritable ",*data.Writable)
+				// fmt.Print("\nISOWN ",*data.IsOwn)
+			}
+		}
+		fmt.Print("SCOPE RESOURCE MAP ", Id_Scope)
+		
+	}
+	fmt.Println("\nONE DONE \n")
 	client.Debugger.Resume(ctx, &debugger.ResumeArgs{})
 	wg.Add(1)
 	go parseBreakpointData(ctx)
