4ca20641385309c031439407572ee6ff6db74a6e
Yogesh Kumar <yogesh.kumar@trilogy.com>
2023-03-15 23:42:24 +0530

    Can pin point location in user's code

--- go-proj/main.go
+++ go-proj/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/go-sourcemap/sourcemap"
 	"github.com/mafredri/cdp"
 	"github.com/mafredri/cdp/devtool"
 	"github.com/mafredri/cdp/protocol/debugger"
@@ -51,10 +54,10 @@ func run(timeout time.Duration) error {
 	curDir, _ := os.Getwd()
 	parDir := filepath.Dir(curDir)
 	urlRegex := "^.*" + parDir + "/my-project2/node_modules/constructs/lib/construct.js$"
-	columnNumber := 9
+	columnNumber := 8
 	client.Debugger.SetBreakpointByURL(ctx, &debugger.SetBreakpointByURLArgs{
 		URLRegex:     &urlRegex,
-		LineNumber:   368,
+		LineNumber:   367,
 		ColumnNumber: &columnNumber,
 	})
 
@@ -89,11 +92,35 @@ func parseBreakpointData(ctx context.Context) error {
 		if callFrame.URL == stackFile || callFrame.URL == mainFile {
 			scriptId := callFrame.Location.ScriptID
 			src, err = client.Debugger.GetScriptSource(ctx, &debugger.GetScriptSourceArgs{ScriptID: scriptId})
-			fmt.Println(src, err)
+			if err != nil {
+				return err
+			}
+
+			mapURL := callFrame.URL
+			splitSrcLines := strings.Split(src.ScriptSource, "\n")
+			sourceMapBase64 := strings.Replace(splitSrcLines[len(splitSrcLines)-1], "//# sourceMappingURL=data:application/json;charset=utf-8;base64,", "", -1)
+
+			sourceMap, err := base64.StdEncoding.DecodeString(sourceMapBase64)
+			if err != nil {
+				return err
+			}
+			smap, err := sourcemap.Parse(mapURL, sourceMap)
+			if err != nil {
+				panic(err)
+			}
+
+			// chrome devtools protocol and it's implementation cdp has both line and column as 0 indexed
+			// For the sourcemap library used is considering line number as 1 indexed and column number as 0 indexed (both input and output)
+			// although their doc mentions that both line and column are 0 indexed
+			// doc: https://docs.google.com/document/d/1U1RGAehQwRypUTovF1KRlpiOFze0b-_2gc6fAH0KY0k/edit
+			genline, gencol := callFrame.Location.LineNumber+1, *callFrame.Location.ColumnNumber
+			file, fn, sourceline, sourcecol, ok := smap.Source(genline, gencol)
+			fmt.Println(file, fn, sourceline, sourcecol, ok)
+			cnt := smap.SourceContent(strings.Replace(mapURL, "file://", "", -1))
+			fmt.Println(cnt)
 		}
 		// for viewing variables
 		var a []*runtime.GetPropertiesReply
-		var b []*runtime.AwaitPromiseReply
 
 		for _, scope := range callFrame.ScopeChain {
 
@@ -103,12 +130,6 @@ func parseBreakpointData(ctx context.Context) error {
 				fmt.Println("Error: ", err)
 			}
 			a = append(a, z)
-
-			y, err := client.Runtime.AwaitPromise(ctx, &runtime.AwaitPromiseArgs{PromiseObjectID: *callFrame.ScopeChain[0].Object.ObjectID})
-			if err != nil {
-				fmt.Println("Error: ", err)
-			}
-			b = append(b, y)
 		}
 
 		// breakpoint here for checking a and b
